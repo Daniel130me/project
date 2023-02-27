fe418cbb4790ec24921912d0a3629601d15adfa3
Oluwagbenga Kosoko <69328757+Daniel130me@users.noreply.github.com>
2023-02-27 15:52:52 +0100

    changed database to sqlite3

--- project/settings.py
+++ project/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3,
+        'NAME': BASE_DIR / 'db.sqlite3',
 #         'USER': 'root',
 #         'PASSWORD': '',
 #         'HOST': '127.0.0.1',
