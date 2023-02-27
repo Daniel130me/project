eea9c0dad0d2f4e3f6f537e67ceb4ba30d17b92d
Oluwagbenga Kosoko <69328757+Daniel130me@users.noreply.github.com>
2023-02-27 15:36:13 +0100

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'kosokodaniel@gmail.com'
-# EMAIL_HOST_PASSWORD = 'ylvbjptkdpveyxp'
+# EMAIL_HOST_USER = 'kosokodfg@gmail.com'
+# EMAIL_HOST_PASSWORD = ''
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
